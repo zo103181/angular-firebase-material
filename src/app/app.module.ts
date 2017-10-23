03b6006f75347058012381f32e47fc1d12927cf8
Zach Olson <zrolson@gmail.com>
2017-10-23 07:45:57 -0500

    Install Angular Material / Icons / Themes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Angular Material Components / Animations
+// -- be sure to import the Angular Material modules 
+// -- after Angular's BrowserModule, as the import order matters
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,9 +14,16 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
+  // creators of services that this module contributes to 
+  // the global collection of services; 
+  // they become accessible in all parts of the app.
   providers: [],
+  // the main application view, called the root component, that 
+  // hosts all other app views. Only the root module should set this bootstrap property.
   bootstrap: [AppComponent]
 })
 export class AppModule { }
