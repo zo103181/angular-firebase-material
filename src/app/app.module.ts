0068ffe3a262a63b1661678df1218d6dc3acac9b
Zach Olson <zrolson@gmail.com>
2017-10-23 12:14:16 -0500

    Initialize Routes Generate Login Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+// Routes
+import { appRoutes } from '../routes';
 
 // Angular Material Components / Animations
 // -- be sure to import the Angular Material modules 
 // -- after Angular's BrowserModule, as the import order matters
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material';
+import {
+  MatButtonModule,
+  MatIconModule,
+  MatInputModule
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    RouterModule.forRoot(appRoutes)
   ],
   // creators of services that this module contributes to 
   // the global collection of services; 
