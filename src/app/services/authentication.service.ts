07c3e7308d08ce05ab2e92d70433cfcb058aa6fc
Zach Olson <zrolson@gmail.com>
2017-12-16 08:31:26 -0600

    Authentication Updates Save custom user data to a Firestore user collection.

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,15 +1,27 @@
 import { Injectable } from '@angular/core';
-import { AngularFireAuth } from 'angularfire2/auth';
+
 import * as firebase from 'firebase/app';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFirestore, AngularFirestoreDocument } from 'angularfire2/firestore';
+
 import { Observable } from 'rxjs/Observable';
 import { User } from '../classes/user.class';
 
 @Injectable()
 export class AuthenticationService {
-  private user: Observable<firebase.User>;
+  public user: Observable<firebase.User>;
 
-  constructor(private afAuth: AngularFireAuth) {
-    this.user = afAuth.authState;
+  constructor(
+    private afAuth: AngularFireAuth,
+    private afs: AngularFirestore
+  ) {
+    this.user = afAuth.authState.switchMap(user => {
+      if (user) {
+        return this.afs.doc<User>(`users/${user.uid}`).valueChanges()
+      } else {
+        return Observable.of(null)
+      }
+    });
   }
 
   authUser() {
@@ -17,7 +29,9 @@ export class AuthenticationService {
   }
 
   login(user: User) {
-    return this.afAuth.auth.signInWithEmailAndPassword(user.email, user.password);
+    return this.afAuth.auth.signInWithEmailAndPassword(user.email, user.password).then((credential) => {
+      this.updateUserData(credential);
+    });
   }
 
   logout() {
@@ -29,6 +43,18 @@ export class AuthenticationService {
   }
 
   signup(user: User) {
-    return this.afAuth.auth.createUserWithEmailAndPassword(user.email, user.password);
+    return this.afAuth.auth.createUserWithEmailAndPassword(user.email, user.password).then((credential) => {
+      this.updateUserData(credential);
+    });
+  }
+
+  private updateUserData(user) {
+    // Sets user data to firestore on login
+    const userRef: AngularFirestoreDocument<any> = this.afs.doc(`users/${user.uid}`);
+    const data: User = {
+      uid: user.uid,
+      email: user.email
+    }
+    return userRef.set(data)
   }
 }
